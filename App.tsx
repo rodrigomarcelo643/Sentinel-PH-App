6a6db8db2f14544aaddb6555f799cec02854be50
Marcelo Rodrigo <rodrigomarcelo643@gmail.com>
2026-02-25 00:49:58 +0800

    feat(test): add test for splash ui

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SplashScreen } from './components/screens/SplashScreen';
+import './global.css';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      <SplashScreen />
+      <StatusBar style="light" />
     </SafeAreaProvider>
   );
 }
