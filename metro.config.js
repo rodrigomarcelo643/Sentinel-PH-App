0ca8d4e8972d7f89321584c90f8cee3a80f0271c
Marcelo Rodrigo <rodrigomarcelo643@gmail.com>
2026-02-25 00:27:30 +0800

    feat: Application Setup

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.unstable_enableSymlinks = true;
+config.resolver.unstable_enablePackageExports = true;
+config.watchFolders = [path.resolve(__dirname)];
 
 module.exports = withNativeWind(config, { input: './global.css' });
